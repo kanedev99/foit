f2be42744793e0454ad07256a80d3baf5b30d1d6
kane <kane@vinova.com.sg>
2020-11-05 10:42:28 +0700

    fix

--- apps/post/models.py
+++ apps/post/models.py
@@ -8,7 +8,6 @@ class Post(models.Model):
     read_time = models.IntegerField(default=0)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
-    created_by_user = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
